d2d7824440254d1e9478204a5125410faea9d871
holczur <holcz.norbert@gmail.com>
2023-07-22 19:16:01 +0200

    practicing databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This coul be a tasty food recipe, but now this is only a test text', 'https://images.pexels.com/photos/1410235/pexels-photo-1410235.jpeg')
+  ];
 }
